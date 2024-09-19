c9a82e251f1f290541e6a115be7461f45ea39828
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-19 20:17:24 +0600

    added AppToolbar, routes;

--- frontend-forum/src/App.tsx
+++ frontend-forum/src/App.tsx
@@ -1,9 +1,29 @@
+import './App.css';
+import AppToolbar from './UI/AppToolbar/AppToolbar';
+import {Route, Routes} from 'react-router-dom';
+import Register from './features/users/components/Register';
+import Login from './features/users/components/Login';
+import NotFoundPage from './UI/NotFoundPage/NotFoundPage';
+import Posts from './features/posts/components/Posts';
+import AddPostPage from './features/posts/AddPostPage';
+
 const App = () => {
-    return (
-        <>
-            Hello World!
-        </>
-    );
+  return (
+    <>
+      <header>
+        <AppToolbar/>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Posts />} />
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/new-post" element={<AddPostPage />} />
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+      </main>
+    </>
+  );
 };
 
 export default App;
