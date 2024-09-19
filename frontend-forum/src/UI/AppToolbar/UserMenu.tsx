704c58592003dca514320224cbc54fa322cd8f51
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-19 20:15:02 +0600

    created UI components NotFoundPage, AnonymousMenu, UserMenu, AppToolbar;

---
+++ frontend-forum/src/UI/AppToolbar/UserMenu.tsx
@@ -0,0 +1,39 @@
+import {Avatar, Box, Button, Menu, MenuItem} from '@mui/material';
+import {User} from '../../types';
+import React, {useState} from 'react';
+import PersonIcon from '@mui/icons-material/Person';
+import {Link} from 'react-router-dom';
+
+interface Props {
+  user: User;
+}
+
+const UserMenu: React.FC<Props> = ({ user }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const isOpen = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box display="flex" alignItems="center">
+      <Avatar sx={{ backgroundColor: '#fff', color: '#448aff', mr: 1 }}>
+        <PersonIcon />
+      </Avatar>
+      <Button onClick={handleClick} color="inherit">
+        {user.username}
+      </Button>
+      <Menu anchorEl={anchorEl} open={isOpen} keepMounted onClose={handleClose}>
+        <MenuItem component={Link} to='/new-post'>Add new post</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </Box>
+  );
+};
+
+export default UserMenu;
