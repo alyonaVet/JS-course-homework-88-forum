63448b473fa5f91686c10ac7a5e50e0861060f6e
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-20 22:20:32 +0600

    added OneComment, CommentForm;

--- frontend-forum/src/features/posts/FullPostPage.tsx
+++ frontend-forum/src/features/posts/FullPostPage.tsx
@@ -6,15 +6,33 @@ import dayjs from 'dayjs';
 import {useParams} from 'react-router-dom';
 import {useEffect} from 'react';
 import {fetchOnePost} from './postsThunk';
+import {addComment, fetchComments} from '../comments/CommentThunk';
+import {selectComments} from '../comments/CommentSlice';
+import OneComment from '../comments/components/OneComment';
+import CommentForm from '../comments/components/CommentForm';
+import {CommentFields} from '../../types';
+import {selectUser} from '../users/usersSlice';
+import {grey} from '@mui/material/colors';
 
 const FullPostPage = () => {
   const dispatch = useAppDispatch();
   const onePost = useAppSelector(selectOnePost);
   const {id} = useParams() as { id: string };
+  const comments = useAppSelector(selectComments);
+  const user = useAppSelector(selectUser);
 
   useEffect(() => {
     dispatch(fetchOnePost(id));
   }, [dispatch, id]);
+
+  useEffect(() => {
+    dispatch(fetchComments(id));
+  }, [dispatch, id]);
+
+  const onFormSubmit = async (comment: CommentFields) => {
+    await dispatch(addComment(comment));
+    await dispatch(fetchComments(id));
+  };
 
   const ImageCardMedia = styled(CardMedia)({
     width: '80%',
@@ -46,6 +64,23 @@ const FullPostPage = () => {
       <Typography variant="subtitle2" component="div" sx={{color: 'text.secondary', textAlign: 'right'}}>
         by {onePost?.user.username}
       </Typography>
+      {comments.map((comment) => (
+        <OneComment
+          key={comment._id}
+          user={comment.user.username}
+          content={comment.content}
+        />
+      ))}
+      {user ? (
+        <CommentForm onSubmit={onFormSubmit} isLoading={false}/>
+      ) : (
+        <Typography
+          variant="body2"
+          sx={{color: grey[500], mt: 3, textAlign: 'center', mb: 3}}
+        >
+          You should be logged in to add the comment.
+        </Typography>
+      )}
     </Container>
   );
 };
