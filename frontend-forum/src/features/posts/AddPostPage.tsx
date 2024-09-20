4fa4ace4f619c87f80fc5be2a93b101ed5c1125e
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-20 13:14:36 +0600

    filled AddPostPage;

--- frontend-forum/src/features/posts/AddPostPage.tsx
+++ frontend-forum/src/features/posts/AddPostPage.tsx
@@ -1,8 +1,36 @@
-const AddPostPage = () => {
-  return (
-    <div>
+import {useAppDispatch, useAppSelector} from '../../app/hooks';
+import {Box, Typography} from '@mui/material';
+import PostForm from './components/PostForm';
+import {addPost, fetchAllPosts} from './postsThunk';
+import {PostFields} from '../../types';
+import {useNavigate} from 'react-router-dom';
+import {selectUser} from '../users/usersSlice';
+import {selectPostsLoading} from './postsSlice';
 
-    </div>
+const AddPostPage = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const user = useAppSelector(selectUser);
+  const postLoading = useAppSelector(selectPostsLoading);
+
+
+  const onFormSubmit = async (post: PostFields) => {
+    await dispatch(addPost(post));
+    await dispatch(fetchAllPosts());
+    navigate('/');
+  };
+
+  return (
+    <Box sx={{m: 4}}>
+      {user ? (
+        <>
+          <Typography variant="h4" mb={5}>Add new post</Typography>
+          <PostForm onSubmit={onFormSubmit} isLoading={postLoading}/>
+        </>
+      ) : (
+        <Typography variant="h4" textAlign={'center'}>You should be logged in to add the post.</Typography>
+      )}
+    </Box>
   );
 };
 
