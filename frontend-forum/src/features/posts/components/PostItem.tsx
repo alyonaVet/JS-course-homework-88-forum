33a74f5eed0fe2df0847126037add8ffff504ae8
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-20 09:42:16 +0600

    created component PostItem;

---
+++ frontend-forum/src/features/posts/components/PostItem.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Box, Card, CardContent, CardMedia, Link, Stack, styled, Typography} from '@mui/material';
+import {apiURL} from '../../../constants';
+import dayjs from 'dayjs';
+import {Link as RouterLink} from 'react-router-dom';
+
+interface Props {
+  user: string;
+  title: string;
+  datetime: string;
+  image: string | null;
+}
+
+const PostItem: React.FC<Props> = ({user, title, image, datetime}) => {
+  const ImageCardMedia = styled(CardMedia)({
+    width: 230,
+    objectFit: 'cover',
+    borderRadius: 'rounded',
+  });
+
+  const cardImage = image ? apiURL + '/' + image : '';
+
+  return (
+    <Card sx={{
+      display: 'flex',
+      width: '100%',
+      height: 150,
+      border: '1px solid #bdbdbd',
+      mb: 4,
+    }}>
+      <Box sx={{display: 'flex', flexDirection: 'row', width: '100%'}}>
+        {image && <ImageCardMedia image={cardImage}/>}
+        <Box sx={{display: 'flex', flexDirection: 'column', flex: 1}}>
+          <CardContent sx={{backgroundColor: '#fafafa', height: '100%'}}>
+            <Stack direction={'row'} mb={3}  ml={3}>
+              <Typography variant="subtitle2" component="div" sx={{color: 'text.secondary'}}>
+                {dayjs(datetime).format('DD.MM.YYYY HH:mm:ss')}
+              </Typography>
+              <Typography variant="subtitle2" component="div" sx={{color: 'text.secondary', ml: 1}}>
+                by {user}
+              </Typography>
+            </Stack>
+            <Link component={RouterLink} to={'/posts/:postId'} variant="h6" noWrap ml={3}>
+              {title}
+            </Link>
+
+          </CardContent>
+        </Box>
+      </Box>
+    </Card>
+  );
+};
+
+export default PostItem;
