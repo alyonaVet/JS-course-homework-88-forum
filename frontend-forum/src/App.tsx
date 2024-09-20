8be05618326a49a6e9c5773a7430835332c6d057
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-20 17:47:22 +0600

    added route path;

--- frontend-forum/src/App.tsx
+++ frontend-forum/src/App.tsx
@@ -5,6 +5,7 @@ import Register from './features/users/components/Register';
 import Login from './features/users/components/Login';
 import NotFoundPage from './UI/NotFoundPage/NotFoundPage';
 import Posts from './features/posts/components/Posts';
+import FullPostPage from './features/posts/FullPostPage';
 import AddPostPage from './features/posts/AddPostPage';
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/register" element={<Register/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/new-post" element={<AddPostPage />} />
+          <Route path="/posts/:id" element={<FullPostPage />} />
           <Route path="*" element={<NotFoundPage/>}/>
         </Routes>
       </main>
