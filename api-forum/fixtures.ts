6fa89d464e7e45daa4339bfcbb8625b7a75572e9
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-20 22:20:46 +0600

    added comments;

--- api-forum/fixtures.ts
+++ api-forum/fixtures.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import config from './config';
 import User from './models/User';
 import Post from './models/Post';
+import Comment from './models/Comment';
 
 const run = async () => {
   await mongoose.connect(config.database);
@@ -9,6 +10,7 @@ const run = async () => {
   try {
     await db.dropCollection('users');
     await db.dropCollection('posts');
+    await db.dropCollection('comments');
   } catch (error) {
     console.log('Collections were not present, skipping drop...');
   }
@@ -25,17 +27,36 @@ const run = async () => {
   );
 
   const [post1, post2] = await Post.create({
-    user: johnUser,
-    title: 'The Serenity of Forests',
-    description: 'Walking through a forest feels like stepping into a world of peace. The rustling leaves and the distant chirps of birds create a soothing symphony that refreshes the soul.',
-    datetime: new Date(),
-    image: 'fixtures/forest.jpeg'
-  }, {
+      user: johnUser,
+      title: 'The Serenity of Forests',
+      description: 'Walking through a forest feels like stepping into a world of peace. The rustling leaves and the distant chirps of birds create a soothing symphony that refreshes the soul.',
+      datetime: new Date(),
+      image: 'fixtures/forest.jpeg'
+    }, {
       user: saraUser,
       title: 'The Beauty of Sunsets',
       description: 'As the sun dips below the horizon, the sky bursts into hues of orange and pink. Each sunset is a gentle reminder of the beauty that nature offers, even at the close of the day.',
       datetime: new Date(),
       image: 'fixtures/sunset.jpeg'
+    },
+  );
+
+  await Comment.create({
+    user: saraUser,
+    post: post1,
+    content: 'Very good post',
+  }, {
+      user: johnUser,
+      post: post1,
+      content: 'Wow, I like it',
+    }, {
+      user: saraUser,
+      post: post2,
+      content: 'I would like to be there',
+    }, {
+      user: johnUser,
+      post: post2,
+      content: 'Amazing post',
     },
   );
 
