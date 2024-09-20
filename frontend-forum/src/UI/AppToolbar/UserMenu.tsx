91069324e789a9766f136e75d63e2f40c6d2684c
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-20 15:23:32 +0600

    added handleLogout;

--- frontend-forum/src/UI/AppToolbar/UserMenu.tsx
+++ frontend-forum/src/UI/AppToolbar/UserMenu.tsx
@@ -3,12 +3,15 @@ import {User} from '../../types';
 import React, {useState} from 'react';
 import PersonIcon from '@mui/icons-material/Person';
 import {Link} from 'react-router-dom';
+import {useAppDispatch} from '../../app/hooks';
+ import {logout} from '../../features/users/usersThunk';
 
 interface Props {
   user: User;
 }
 
-const UserMenu: React.FC<Props> = ({ user }) => {
+const UserMenu: React.FC<Props> = ({user}) => {
+  const dispatch = useAppDispatch();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const isOpen = Boolean(anchorEl);
 
@@ -20,17 +23,21 @@ const UserMenu: React.FC<Props> = ({ user }) => {
     setAnchorEl(null);
   };
 
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <Box display="flex" alignItems="center">
-      <Avatar sx={{ backgroundColor: '#fff', color: '#448aff', mr: 1 }}>
-        <PersonIcon />
+      <Avatar sx={{backgroundColor: '#fff', color: '#448aff', mr: 1}}>
+        <PersonIcon/>
       </Avatar>
       <Button onClick={handleClick} color="inherit">
         {user.username}
       </Button>
       <Menu anchorEl={anchorEl} open={isOpen} keepMounted onClose={handleClose}>
-        <MenuItem component={Link} to='/new-post'>Add new post</MenuItem>
-        <MenuItem>Logout</MenuItem>
+        <MenuItem component={Link} to="/new-post">Add new post</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
       </Menu>
     </Box>
   );
